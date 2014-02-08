4e2a62ebe11e6787c69322ef847d39e869a48f26
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-02-08 22:28:35 +0900

    add a test for xmopen

--- main.c
+++ main.c
@@ -1,7 +1,31 @@
 #include "xfile.h"
+#include <stdio.h>
+#include <assert.h>
+
+void
+test_memio(void)
+{
+  XFILE *mem;
+  char c;
+
+  mem = xmopen();
+
+  /* output */
+  xfprintf(mem, "%d %s %c\n", 42, "hello", 'A');
+
+  /* input */
+  while ((c = xfgetc(mem)) != -1) {
+    putchar(c);
+  }
+  puts(" equals '42 hello A\n'?");
+
+  assert(xfclose(mem) == 0);
+}
 
 int
 main()
 {
   xprintf("%d %d %d\n", 1, 42, -100);
+
+  test_memio();
 }
